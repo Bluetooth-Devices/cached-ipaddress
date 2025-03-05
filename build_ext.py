9db200cd738c7b1db3b651bb03bfb4756386d87e
J. Nick Koston <nick@koston.org>
2025-03-04 16:00:28 -1000

    feat: reduce size of wheels (#53)
    compile with -g0 to reduce size of wheels

--- build_ext.py
+++ build_ext.py
@@ -5,6 +5,24 @@ import os
 from distutils.command.build_ext import build_ext
 from typing import Any
 
+try:
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import Extension
+
+TO_CYTHONIZE = ["src/cached_ipaddress/ipaddress.py"]
+
+EXTENSIONS = [
+    Extension(
+        ext.removeprefix("src/").removesuffix(".py").replace("/", "."),
+        [ext],
+        language="c",
+        extra_compile_args=["-O3", "-g0"],
+    )
+    for ext in TO_CYTHONIZE
+]
+
+
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -30,9 +48,7 @@ def build(setup_kwargs: Any) -> None:
         setup_kwargs.update(
             {
                 "ext_modules": cythonize(
-                    [
-                        "src/cached_ipaddress/ipaddress.py",
-                    ],
+                    EXTENSIONS,
                     compiler_directives={"language_level": "3"},  # Python 3
                 ),
                 "cmdclass": {"build_ext": BuildExt},
